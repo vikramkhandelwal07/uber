c50207ca49041723804c9967e940b5fe653be815
vikram khandelwal <vikramkhandelwal07@gmail.com>
2025-01-05 17:05:09 +0530

    updated server .js

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const app = require('./app');
 const { initializeSocket } = require('./socket');
 const port = process.env.PORT || 3000;
+// creating server
 
 const server = http.createServer(app);
 
